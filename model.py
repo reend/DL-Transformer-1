f4957116cad91fe362c5cea7fea3b4b7544d5fc6
K.P <prilipko.k@yoju.casino>
2024-01-20 01:09:32 +0100

    positional encoding init

--- model.py
+++ model.py
@@ -9,6 +9,27 @@ class InputEmbeddings(nn.Module):
         self.d_model = d_model
         self.vocab_size = vocab_size
         self.embedding = nn.Embedding(vocab_size, d_model)
-    
-    def forward(self, x):
+
+    def forward(self, x): 
         return self.embedding(x) * math.sqrt(self.d_model)
+
+class PositionalEncoding(nn.Module):
+
+    def __init__(self, d_model: int, seq_len: int, dropout: float) -> None:
+        super().__init__()
+        self.d_model = d_model
+        self.seq_len = seq_len
+        self.dropout = nn.Dropout(dropout)
+
+        # positional encoding matrix (seq_len, d_model)
+        pe = torch.zeros(seq_len, d_model) 
+        # vector (seq_len, 1)
+        position = torch.arange(0, seq_len, dtype=torch.float).unsqueeze(1) 
+        # formula for positional encoding
+        div_term = torch.exp(torch.arange(0, d_model, 2).float() * (-math.log(10000.0) / d_model)) 
+        pe[:, 0::2] = torch.sin(position * div_term) # even indices
+        pe[:, 1::2] = torch.cos(position * div_term) # odd indices
+
+        pe = pe.unsqueeze(0) # (1, seq_len, d_model)
+
+        self.register_buffer('pe', pe) # register pe as a buffer to be used in forward pass
